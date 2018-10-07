051834f3427e28c09c9a27880d71738aec4d3a46
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 16:04:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,6 +61,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != []
+      return self.find_by_name(name)
+    else
+      self.create(name)
+    end
   end
 
 
