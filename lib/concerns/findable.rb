adaeec94314fee64d6d0623ab7ffd4e590798016
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 16:20:23 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,4 +3,14 @@ module Concerns::Findable
     results = self.all.select { |song| song.name == name}
     results[0]
   end
+
+  def self.find_or_create_by_name(name)
+    find_results = self.find_by_name(name)
+    if find_results != nil
+      return find_results
+    else
+      new_song = self.create(name)
+      return new_song
+    end
+  end
 end
