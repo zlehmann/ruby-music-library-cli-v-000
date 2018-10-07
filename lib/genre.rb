5b9c4b3e5d1b586055ddb74732b04306cb5eef49
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 14:50:34 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,31 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    return genre
+  end
+
+end
