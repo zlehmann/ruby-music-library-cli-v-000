ceabc0a48b2859dc5155ddde8ccc5917cded1bd0
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 16:20:00 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
   def find_by_name(name)
-    results = class.all.select { |song| song.name == name}
+    results = self.all.select { |song| song.name == name}
     results[0]
   end
 end
