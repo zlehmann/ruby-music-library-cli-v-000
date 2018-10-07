f47a965c80403129f7819c1781512b732335ba12
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 15:59:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,10 +55,11 @@ class Song
     end
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    @@all.select { |song| song.name == name}
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
   end
 
 
