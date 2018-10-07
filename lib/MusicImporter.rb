a9d298e2a3ab16be07807f8e47417634491b048c
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 16:23:41 +0000

    automatically backed up by learn

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,19 @@
+class MusicImporter
+  attr_accessor :filepath
+
+  def initialize(filepath)
+    @filepath = filepath
+  end
+
+  def path
+    @filepath
+  end
+
+  def files
+
+  end
+
+  def self.import
+
+  end
+end 
