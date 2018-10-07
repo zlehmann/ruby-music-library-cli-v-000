ad5aa657dafba62c00710144dfe57c7047ceebee
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 14:49:19 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    return artist
+  end
+
+end
